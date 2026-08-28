c45d226b69f3b9202b99e0841182826cb4ac2963
jaywang-cpu <jaywangcpu@gmail.com>
2026-08-28 17:44:57 -0400

    符号系统并入思维模型页：形式模型下的三格宽大卡，删掉独立的 signals 页

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const V = 'mmb-v8';
-const FILES = ['./', './index.html', './apple-touch-icon.png', './icon-192.png', './icon-512.png', './models.html', './polya.html', './signals.html', './drill.html', './vendor/pdf.min.mjs', './vendor/pdf.worker.min.mjs',
+const V = 'mmb-v9';
+const FILES = ['./', './index.html', './apple-touch-icon.png', './icon-192.png', './icon-512.png', './models.html', './polya.html', './drill.html', './vendor/pdf.min.mjs', './vendor/pdf.worker.min.mjs',
                './icon.svg', './manifest.webmanifest'];
 
 self.addEventListener('install', e => {
